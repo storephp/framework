b1882e4a5d5f5267bdfc6904d8dbd3f179d99105
Karim Mohamed <komicho1996@gmail.com>
2022-12-15 12:58:57 +0200

    Feat: install livewire and use it

---
+++ src/Http/Livewire/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bidaea\OutMart\Dashboard\Http\Livewire;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('outmart::home')->layout('outmart::layouts.dashboard');
+    }
+}
--- src/OutMartDashboardServiceProvider.php
+++ src/OutMartDashboardServiceProvider.php
@@ -23,6 +23,8 @@ class OutMartDashboardServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // 
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'outmart');
     }
 }

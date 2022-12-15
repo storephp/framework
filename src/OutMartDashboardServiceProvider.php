fb6ddb8964de126aee3d72f9b12fe2e67a546635
Karim Mohamed <komicho1996@gmail.com>
2022-12-15 12:40:30 +0200

    first commit

---
+++ src/OutMartDashboardServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bidaea\OutMart\Dashboard;
+
+use Illuminate\Support\ServiceProvider;
+
+class OutMartDashboardServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 
+    }
+}

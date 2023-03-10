4625384a1ac7892abc1b6ef96445982b5b7a5fba
Karim Mohamed <komicho1996@gmail.com>
2023-03-10 14:33:17 +0200

    Add `WithBasket` trait to `Customer` model

--- modules/Customers/Models/Customer.php
+++ modules/Customers/Models/Customer.php
@@ -3,9 +3,12 @@
 namespace OutMart\Modules\Customers\Models;
 
 use OutMart\Models\Customer as OurMartCustomer;
+use OutMart\Traits\Customer\WithBasket;
+
 // use OutMart\Traits\Customer\Initialize\WithUserData;
 
 final class Customer extends OurMartCustomer
 {
     // use WithUserData;
+    use WithBasket;
 }

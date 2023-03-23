289c69e6b28d943fd5aaa4c151aef14da86ccbf4
Karim Mohamed <komicho1996@gmail.com>
2023-03-24 01:04:25 +0200

    Fix: enable home page

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use OutMart\Dashboard\Http\Livewire\Catalog\Categories\CategoriesIndex;
-use OutMart\Dashboard\Http\Livewire\Catalog\Categories\CategoryCreate;
-use OutMart\Dashboard\Http\Livewire\Catalog\Categories\CategoryEdit;
-use OutMart\Dashboard\Http\Livewire\Customers\CustomersIndex;
+use Illuminate\Support\Facades\Route;
 use OutMart\Dashboard\Http\Livewire\Home;
 use OutMart\Dashboard\Http\Middleware\GlobalConfigMiddleware;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,25 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function(){
-    throw new Exception('Hi');
-});
-
-
-Route::prefix('outmart')->middleware(['web', GlobalConfigMiddleware::class])->group(function () {
+Route::prefix('outmart')->middleware(['web', 'auth', GlobalConfigMiddleware::class])->group(function () {
     Route::get('/', Home::class)->name('outmart.dashboard.home');
-
-    // // catalog
-    // Route::prefix('catalog')->group(function () {
-    //     Route::prefix('categories')->group(function () {
-    //         Route::get('/', CategoriesIndex::class)->name('outmart.dashboard.catalog.categories.index');
-    //         Route::get('/create', CategoryCreate::class)->name('outmart.dashboard.catalog.categories.create');
-    //         Route::get('/{category}/edit', CategoryEdit::class)->name('outmart.dashboard.catalog.categories.edit');
-    //     });
-    // });
-
-    // // Customers
-    Route::prefix('customers')->group(function () {
-        Route::get('/', CustomersIndex::class)->name('outmart.dashboard.customers.index');
-    });
 });
--- src/Http/Livewire/Home.php
+++ src/Http/Livewire/Home.php
@@ -3,11 +3,12 @@
 namespace OutMart\Dashboard\Http\Livewire;
 
 use Livewire\Component;
+use OutMart\Dashboard\Views\Layouts\DashboardLayout;
 
 class Home extends Component
 {
     public function render()
     {
-        return view('outmart::home')->layout('outmart::layouts.dashboard');
+        return view('outmart::home')->layout(DashboardLayout::class);
     }
 }

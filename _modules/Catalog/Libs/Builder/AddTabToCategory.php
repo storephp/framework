2fa4b216d435a07e906b516b57ba2c5655383859
Karim Mohamed <komicho1996@gmail.com>
2023-09-03 23:40:19 +0300

    Refactor: remove the old catalog module

--- _modules/Catalog/Libs/Builder/AddTabToCategory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Store\Modules\Catalog\Libs\Builder;
-
-use StorePHP\Dashboard\Builder\Abstracts\AppendTabs;
-
-class AddTabToCategory extends AppendTabs
-{
-}
--- _modules/Catalog/lang/en/menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'catalog' => 'Catalog',
-    'categories' => 'Categories',
-];
